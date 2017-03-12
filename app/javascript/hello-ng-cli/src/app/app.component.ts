e4c9dd9e4073e48ddfe2884195d1f31f01273ba0
smapira <info@routeflags.com>
2017-03-13 01:18:05 +0900

    update app.component.ts

--- app/javascript/hello-ng-cli/src/app/app.component.ts
+++ app/javascript/hello-ng-cli/src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'hello-ng-cli',
+    template: require('./app.component.html'),
 })
 export class AppComponent {
-  title = 'app works!';
+    title = 'app works!';
 }
